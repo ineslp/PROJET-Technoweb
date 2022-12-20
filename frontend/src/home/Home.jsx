da053764264641ce5440dd19cf2ce07453af62b7
ineslp <78728423+ineslp@users.noreply.github.com>
2022-12-21 00:39:32 +0100

    Add files via upload

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,21 +1,22 @@
 import React from "react"
+import Followers from "../components/followers/Followers"
 import "./home.css"
+import Chart from "../components/chart/Chart"
+import People from "../components/people/People"
+import Localisation from "../components/localisation/Localisation"
+
+
+
 
 export default function Home() {
     return (
 
 
         <div className="home">
-
-            <div className="followers">
-                Chargement...
-        
-            </div>
-
-
-            <div className="topfollowers">
-
-            </div>
+                <Followers/>
+                <Chart/>
+                <People/>
+                <Localisation/>
         </div>
     )
 
