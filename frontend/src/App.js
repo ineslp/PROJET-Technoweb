9dcc002b4266ea2d5fc8cae1585a68ce587d0d87
ineslp <78728423+ineslp@users.noreply.github.com>
2022-12-21 00:36:00 +0100

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,22 +16,5 @@ function App() {
   );
 }
 
-/*Widget followers @instagram*/
-const followers= document.getElementById('followers');
-async function getFollowers(profileId){
-  const url= 'https://instagram.com/${profileId}/?__a=1';
-  const response= await fetch(url);
-  const data=await response.json();
-
-  return data.graphql.user.edge_followed_by.count;
-}
-async function setFollowers(profileId) {
-  followers.textContent = await getFollowers(profileId);
-  console.log(await getFollowers(profileId));
-}
-setInterval(()=>
-setFollowers('instagram'),1000);
-
-
 
 export default App;
